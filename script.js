75857a66a56b08cfc03d8f1f2a2e5c9672971630
Pat <27511950+ThePat02@users.noreply.github.com>
2019-04-03 19:04:19 +0200

    Moved export functions to another file

--- script.js
+++ script.js
@@ -1,48 +1,4 @@
-function download(filename, text) {
-  var element = document.createElement('a');
-  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text.replace(/\n/gmi, '\r\n')));
-  element.setAttribute('download', filename);
-
-  element.style.display = 'none';
-  document.body.appendChild(element);
-
-  element.click();
-
-  document.body.removeChild(element);
-}
-
-function exportTxt() {
-  download(document.getElementById("titel").value + ".txt", document.getElementById("text").value.replace(/\n/gmi, '\r\n'));
-}
-
-function exportHtml() {
-  var source = tagsToElements(document.getElementById("text").value.replace(/\n/gmi, '<br>'));
-  download(document.getElementById("titel").value + ".html", source);
-}
-
-function exportPdf() {
-  var doc = new jsPDF();
-
-  var elementHandler = {
-    '#ignorePDF': function(element, renderer) {
-      return true;
-    }
-  };
-
-  var source = getSource();
-  alert(source);
-  doc.fromHTML(
-    source,
-    15,
-    15, {
-      'width': 180,
-      'elementHandlers': elementHandler
-    });
-
-  doc.setFont("arial");
-
-  doc.save(document.getElementById("titel").value);
-}
+var htmlprestyle = "<style>html{font-family:Arial}</style> ";
 
 function saving() {
   localStorage.setItem("titel", document.getElementById("titel").value);
@@ -56,36 +12,7 @@ function saving() {
   });
 }
 
-function getSource() {
-  var source = document.getElementById("text").value;
-  source = tagsToElements(source);
-  source = source.replace(/\n/gmi, '<br> ');
-  return source;
-}
 
-function tagsToElements(a) {
-  var output = a;
-  output = htmlprestyle + output;
-  output = output.replace("((", "<strong>");
-  output = output.replace("))", "</strong>");
-  output = output.replace("(~", "<i>");
-  output = output.replace("~)", "</i>");
-  output = output.replace("--->", "<h3>");
-  output = output.replace("<---", "</h3>");
-  output = output.replace("-->", "<h2>");
-  output = output.replace("<--", "</h3>");
-  output = output.replace("->", "<h1>");
-  output = output.replace("<-", "</h1>");
-
-
-
-  return output;
-}
-
-function newFile() {
-  localStorage.clear();
-  location.reload();
-}
 
 
 $(document).ready(function() {
@@ -96,6 +23,3 @@ $(document).ready(function() {
     saving();
   });
 });
-
-
-var htmlprestyle = "<style>html{font-family:Arial}</style> ";
